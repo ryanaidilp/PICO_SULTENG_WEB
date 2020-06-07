275d83646b6c3dbce97c42d2fca772f5a4ae4112
RyanAidilPratama <fajrianap25@gmail.com>
2020-06-08 07:10:44 +0800

    Update Migrations

--- database/migrations/2020_05_16_154313_create_stats_table.php
+++ database/migrations/2020_05_16_154313_create_stats_table.php
@@ -8,21 +8,30 @@ class CreateStatsTable extends Migration
 {
     /**
      * Run the migrations.
-     *
-     * @return void
      */
     public function up()
     {
         Schema::create('stats', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('day');
+            $table->string('date');
+            $table->integer('positive');
+            $table->integer('recovered');
+            $table->integer('death');
+            $table->integer('negative')->default(0);
+            $table->integer('new_ODP')->default(0);
+            $table->integer('new_PDP')->default(0);
+            $table->integer('finished_ODP')->default(0);
+            $table->integer('finished_PDP')->default(0);
+            $table->double('Rt')->default(0);
+            $table->double('Rt_upper')->default(0);
+            $table->double('Rt_lower')->default(0);
             $table->timestamps();
         });
     }
 
     /**
      * Reverse the migrations.
-     *
-     * @return void
      */
     public function down()
     {

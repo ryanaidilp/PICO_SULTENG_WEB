a107ad6ae7d21ab3001501ca10c48e7425ed4d7c
RyanAidilPratama <fajrianap25@gmail.com>
2020-06-15 14:16:21 +0800

    feat : add hospital list & infographics list in home

--- resources/js/components/pages/HomeComponent.vue
+++ resources/js/components/pages/HomeComponent.vue
@@ -17,19 +17,120 @@
         <i class="fas fa-angle-right" aria-hidden="true"></i>
       </router-link>
     </div>
+    <section class="m-4 mt-4 md:mt-8">
+      <div class="flex flex-col items-stretch p-5 my-4 bg-white rounded-lg shadow-md">
+        <h3 class="text-lg lg:text-2xl">
+          <strong>Daftar Rumah Sakit Rujukan di Sulawesi Tengah</strong>
+        </h3>
+        <br />
+        <p class="opacity-75">
+          Berikut ini adalah rumah sakit yang menjadi rujukan untuk pasien dengan status Pasien dalam Pengawasan.
+          Anda harus mengunjungi fasilitas kesehatan terdekat terlebih dahulu seperti klinik/rumah sakit umum sebelum akhirnya dapat dirujuk ke rumah sakit khusus di bawah ini.
+        </p>
+        <br />
+        <br />
+        <ul class="mt-8 hospital-list">
+          <li v-for="(h, index) in jsonDataRumahSakit" :key="index">
+            <ContactListItem
+              :nama="h.nama"
+              :alamat="h.alamat"
+              :email="h.email"
+              :telepon="h.telepon"
+            />
+          </li>
+        </ul>
+        <br />
+        <router-link
+          tag="a"
+          to="/corona/kontak"
+          class="px-4 py-2 text-center text-blue-700 border-2 border-blue-500 border-solid rounded-lg md:self-center hover:bg-blue-200"
+        >Lihat Rumah Sakit Lainnya</router-link>
+      </div>
+    </section>
+    <section class="m-4 mt-8 md:mt-16">
+      <div class="bg-white rounded-lg shadow-md">
+        <div class="flex flex-col flex-wrap p-5 items-left md:p-8">
+          <h2 class="text-2xl text-left md:text-3xl">
+            <b>Info Praktikal</b>
+          </h2>
+          <p class="opacity-75">
+            Berikut ini adalah beberapa infografis seputar COVID-19.
+            <br />Sumber :
+            <a
+              target="_blank"
+              href="https://instagram.com/sultenglawancovid"
+              class="hover:underline hover:opacity-100"
+            >Sulteng Lawan COVID</a>.
+          </p>
+        </div>
+        <ShareableItems :items="shareableInfographics" />
+        <div class="pb-5 text-center md:pb-8">
+          <router-link
+            class="inline-block px-4 py-2 mt-8 text-center text-blue-800 border-2 border-blue-500 border-solid rounded-lg md:self-center hover:bg-blue-200"
+            to="/corona/infografis"
+          >Lihat Selengkapnya</router-link>
+        </div>
+      </div>
+    </section>
     <Partner class="mt-16" />
   </div>
 </template>
 <script>
-import DataCovid from "../partial/home/DataCovid"
+import DataCovid from "../partial/home/DataCovid";
 export default {
   components: {
+    ShareableItems: () =>
+      import("../partial/infographics/ShareableItems/index"),
+    ContactListItem: () => import("../partial/ContactListItem/index"),
     CarouselComponent: () => import("../partial/home/CarouselComponent"),
     CallCenterComponent: () => import("../partial/home/CardCallCenter"),
     Donation: () => import("../partial/home/DonationCard"),
     DataCovid,
     InfoCovid: () => import("../partial/home/InfoCovid"),
     Partner: () => import("../partial/PartnerFooter")
+  },
+  data() {
+    return {
+      shareableInfographics: [],
+      jsonDataRumahSakit: []
+    };
+  },
+  methods: {
+    fetchDataRumahSakit() {
+      axios.get("/corona/api/rumahsakit").then(response => {
+        let data = response.data.data;
+        for (let i = 0; i < 3; i++) {
+          const element = data[i];
+          this.jsonDataRumahSakit.push(element);
+        }
+      });
+    },
+    fetchDataInfografis() {
+      axios.get("/corona/api/infografis").then(response => {
+        let data = response.data.data;
+        let infographics = [];
+        for (let i = 0; i < 10; i++) {
+          const element = data[i];
+          infographics.push(element);
+        }
+        this.shareableInfographics = infographics;
+      });
+    }
+  },
+  created() {
+    this.fetchDataRumahSakit();
+    this.fetchDataInfografis();
   }
 };
 </script>
+
+<style lang="scss" scoped>
+.hospital-list {
+  display: block;
+  @screen lg {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    align-items: stretch;
+  }
+}
+</style>

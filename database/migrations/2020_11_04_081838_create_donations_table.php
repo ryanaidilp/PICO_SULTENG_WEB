ca0b7a889605c21dfd51f43e4cc74b5b643a823a
ryanaidilp <fajrianaidilp@gmail.com>
2020-11-12 23:08:07 +0800

    ref : add Donation - make Donation model with migrations, controller, factory, and seeder

---
+++ database/migrations/2020_11_04_081838_create_donations_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDonationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('donations', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama'); //name
+            $table->string('phone');
+            $table->string('image');
+            $table->bool('status'); //determine  the donation is no longer active
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('donations');
+    }
+}

4186649716b54476bf38e8ba1abcc993a4a01be9
ryanaidilp <fajrianaidilp@gmail.com>
2020-12-28 19:48:40 +0800

    chore : rebuild ui component

---
+++ public/chunks/3.js
@@ -0,0 +1,236 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[3],{
+
+/***/ "./node_modules/vue-loader/lib/loaders/templateLoader.js?!./node_modules/vue-loader/lib/index.js?!./resources/js/components/CardLindungi.vue?vue&type=template&id=ccb0086e&":
+/*!***************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/vue-loader/lib??vue-loader-options!./resources/js/components/CardLindungi.vue?vue&type=template&id=ccb0086e& ***!
+  \***************************************************************************************************************************************************************************************************************/
+/*! exports provided: render, staticRenderFns */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "render", function() { return render; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "staticRenderFns", function() { return staticRenderFns; });
+var render = function() {
+  var _vm = this
+  var _h = _vm.$createElement
+  var _c = _vm._self._c || _h
+  return _c("div", { staticClass: "w-full mt-4" }, [
+    _c("div", { staticClass: "p-6 mx-4 bg-white rounded-lg shadow-lg" }, [
+      _c("h3", { staticClass: "text-xl font-bold lg:text-2xl" }, [
+        _vm._v("Lindungi Diri dan Orang Lain")
+      ]),
+      _vm._v(" "),
+      _c("div", { staticClass: "flex flex-row w-full" }, [
+        _vm._m(0),
+        _vm._v(" "),
+        _c(
+          "div",
+          {
+            staticClass:
+              "flex-col justify-center hidden w-1/4 mx-2 my-auto rounded-lg md:flex lg:block sm:p-2 lg:mx-16"
+          },
+          [
+            _c("img", {
+              directives: [
+                {
+                  name: "lazy",
+                  rawName: "v-lazy",
+                  value: _vm.route("home") + "assets/images/wash_hands.png",
+                  expression: "route('home')+'assets/images/wash_hands.png'"
+                }
+              ],
+              staticClass: "object-contain",
+              attrs: { title: "Cuci Tangan", alt: "Cuci Tangan" }
+            }),
+            _vm._v(" "),
+            _c("img", {
+              directives: [
+                {
+                  name: "lazy",
+                  rawName: "v-lazy",
+                  value: _vm.route("home") + "assets/images/wear_mask.png",
+                  expression: "route('home')+'assets/images/wear_mask.png'"
+                }
+              ],
+              staticClass: "object-contain mt-6",
+              attrs: { title: "Pakai Masker", alt: "Pakai Masker" }
+            })
+          ]
+        )
+      ]),
+      _vm._v(" "),
+      _vm._m(1)
+    ])
+  ])
+}
+var staticRenderFns = [
+  function() {
+    var _vm = this
+    var _h = _vm.$createElement
+    var _c = _vm._self._c || _h
+    return _c(
+      "span",
+      { staticClass: "w-auto mt-4 text-xs text-justify lg:w-3/4 lg:text-base" },
+      [
+        _vm._v(
+          "\n        Sampai saat ini belum ditemukan vaksin untuk COVID-19, sehingga cara terbaik agar tidak terinfeksi adalah dengan menghindari terekspos virus.\n        "
+        ),
+        _c("br"),
+        _vm._v(" "),
+        _c("br"),
+        _vm._v(" "),
+        _c("span", { staticClass: "font-semibold" }, [
+          _vm._v(
+            "COVID-19 dapat menyebar dari orang ke orang melalui tetesan kecil (droplet) saat batuk atau bersin. Maka yang bisa Komiu dan Kita semua lakukan adalah:"
+          )
+        ]),
+        _vm._v(" "),
+        _c("ul", { staticClass: "mt-2" }, [
+          _c("li", [
+            _c("i", {
+              staticClass: "text-green-400 fas fa-check",
+              attrs: { "aria-hidden": "true" }
+            }),
+            _vm._v(" Sering cuci tangan pakai sabun atau\n            "),
+            _c("i", [_vm._v("Hand Sanitizer")])
+          ]),
+          _vm._v(" "),
+          _c("li", [
+            _c("i", {
+              staticClass: "text-green-400 fas fa-check",
+              attrs: { "aria-hidden": "true" }
+            }),
+            _vm._v(
+              " Hindari menyentuh wajah terutama area hidung, mulut, dan mata\n          "
+            )
+          ]),
+          _vm._v(" "),
+          _c("li", [
+            _c("i", {
+              staticClass: "text-green-400 fas fa-check",
+              attrs: { "aria-hidden": "true" }
+            }),
+            _vm._v(
+              " Bersihkan permukaan benda yang disentuh banyak orang\n          "
+            )
+          ]),
+          _vm._v(" "),
+          _c("li", [
+            _c("i", {
+              staticClass: "text-green-400 fas fa-check",
+              attrs: { "aria-hidden": "true" }
+            }),
+            _vm._v(" "),
+            _c("i", [_vm._v("Physical Distancing")]),
+            _vm._v("! Minimalisir kontak fisik dengan sesama\n          ")
+          ]),
+          _vm._v(" "),
+          _c("li", [
+            _c("i", {
+              staticClass: "text-green-400 fas fa-check",
+              attrs: { "aria-hidden": "true" }
+            }),
+            _vm._v(" Jaga jarak 1-3 meter dengan sesama\n          ")
+          ]),
+          _vm._v(" "),
+          _c("li", [
+            _c("i", {
+              staticClass: "text-green-400 fas fa-check",
+              attrs: { "aria-hidden": "true" }
+            }),
+            _vm._v(
+              " Tetap tinggal di rumah, hindari bepergian ke tempat umum bila tidak perlu\n          "
+            )
+          ]),
+          _vm._v(" "),
+          _c("li", [
+            _c("i", {
+              staticClass: "text-green-400 fas fa-check",
+              attrs: { "aria-hidden": "true" }
+            }),
+            _vm._v(
+              " Selalu gunakan masker jika harus keluar rumah dan berinteraksi langsung dengan orang lain\n          "
+            )
+          ])
+        ])
+      ]
+    )
+  },
+  function() {
+    var _vm = this
+    var _h = _vm.$createElement
+    var _c = _vm._self._c || _h
+    return _c("i", { staticClass: "mt-8 text-xs text-gray-600 " }, [
+      _vm._v("\n        Sumber materi :\n        "),
+      _c(
+        "a",
+        {
+          staticClass: "no-underline hover:no-underline hover:text-blue-200",
+          attrs: { href: "https://pikobar.jabarprov.go.id" }
+        },
+        [_vm._v("https://pikobar.jabarprov.go.id")]
+      )
+    ])
+  }
+]
+render._withStripped = true
+
+
+
+/***/ }),
+
+/***/ "./resources/js/components/CardLindungi.vue":
+/*!**************************************************!*\
+  !*** ./resources/js/components/CardLindungi.vue ***!
+  \**************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _CardLindungi_vue_vue_type_template_id_ccb0086e___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./CardLindungi.vue?vue&type=template&id=ccb0086e& */ "./resources/js/components/CardLindungi.vue?vue&type=template&id=ccb0086e&");
+/* harmony import */ var _node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../../../node_modules/vue-loader/lib/runtime/componentNormalizer.js */ "./node_modules/vue-loader/lib/runtime/componentNormalizer.js");
+
+var script = {}
+
+
+/* normalize component */
+
+var component = Object(_node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_1__["default"])(
+  script,
+  _CardLindungi_vue_vue_type_template_id_ccb0086e___WEBPACK_IMPORTED_MODULE_0__["render"],
+  _CardLindungi_vue_vue_type_template_id_ccb0086e___WEBPACK_IMPORTED_MODULE_0__["staticRenderFns"],
+  false,
+  null,
+  null,
+  null
+  
+)
+
+/* hot reload */
+if (false) { var api; }
+component.options.__file = "resources/js/components/CardLindungi.vue"
+/* harmony default export */ __webpack_exports__["default"] = (component.exports);
+
+/***/ }),
+
+/***/ "./resources/js/components/CardLindungi.vue?vue&type=template&id=ccb0086e&":
+/*!*********************************************************************************!*\
+  !*** ./resources/js/components/CardLindungi.vue?vue&type=template&id=ccb0086e& ***!
+  \*********************************************************************************/
+/*! exports provided: render, staticRenderFns */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_CardLindungi_vue_vue_type_template_id_ccb0086e___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! -!../../../node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!../../../node_modules/vue-loader/lib??vue-loader-options!./CardLindungi.vue?vue&type=template&id=ccb0086e& */ "./node_modules/vue-loader/lib/loaders/templateLoader.js?!./node_modules/vue-loader/lib/index.js?!./resources/js/components/CardLindungi.vue?vue&type=template&id=ccb0086e&");
+/* harmony reexport (safe) */ __webpack_require__.d(__webpack_exports__, "render", function() { return _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_CardLindungi_vue_vue_type_template_id_ccb0086e___WEBPACK_IMPORTED_MODULE_0__["render"]; });
+
+/* harmony reexport (safe) */ __webpack_require__.d(__webpack_exports__, "staticRenderFns", function() { return _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_CardLindungi_vue_vue_type_template_id_ccb0086e___WEBPACK_IMPORTED_MODULE_0__["staticRenderFns"]; });
+
+
+
+/***/ })
+
+}]);

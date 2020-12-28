89829d6ab6e3c4209561936131fdaabe71d12cbb
ryanaidilp <fajrianaidilp@gmail.com>
2020-12-28 19:48:11 +0800

    chore : add how to prevent covid component

---
+++ resources/js/components/_pages/wiki/HowToPreventCovid.vue
@@ -0,0 +1,94 @@
+<template>
+  <div class="p-6 mx-4 bg-white rounded-lg shadow-lg">
+    <h3 class="text-lg font-bold text-left lg:text-3xl">
+      Bagaimana cara mencegah COVID-19?
+    </h3>
+    <p class="mt-4 text-sm md:text-base">
+      Hingga saat ini, belum ada vaksin untuk mencegah penularan COVID-19. <br /> Salah
+      satu cara yang bisa dilakukan untuk mencegah tertularnya virus ini adalah:
+    </p>
+    <ul class="mt-4 text-xs md:text-base">
+      <li class="flex flex-row">
+        <p class="mr-2">1.</p>
+        <p>
+          Menjaga kesehatan dan kebugaran agar sistem imunitas/kekebalan tubuh
+          meningkat.
+        </p>
+      </li>
+      <li class="flex flex-row">
+        <p class="mr-2">2.</p>
+        <p>
+          Mencuci tangan menggunakan air dan sabun atau hand-rub berbasis
+          alkohol. Mencuci tangan sampai bersih selain dapat membunuh virus yang
+          mungkin ada di tangan kita, tindakan ini juga merupakan salah satu
+          tindakan yang mudah dan murah. Sekitar 98% penyebaran penyakit
+          bersumber dari tangan. Karena itu, menjaga kebersihan tangan adalah
+          hal yang sangat penting.
+        </p>
+      </li>
+      <li class="flex flex-row">
+        <p class="mr-2">3.</p>
+        <p>
+          Ketika batuk dan bersin, upayakan menjaga agar lingkungan Anda tidak
+          tertular. Tutup hidung dan mulut Anda dengan tisu atau dengan lengan
+          (bukan dengan telapak tangan).
+        </p>
+      </li>
+      <li class="flex flex-row">
+        <p class="mr-2">4.</p>
+        <p>
+          Menjaga jarak saat berbicara dengan orang lain, sekurang- kurangnya
+          satu meter, terutama dengan orang yang sedang menderita batuk,
+          pilek/bersin dan demam. Saat seseorang terinfeksi penyakit saluran
+          pernafasan, seperti 2019-nCoV, batuk/bersin dapat menghasilkan droplet
+          yang mengandung virus. Jika kita terlalu dekat, virus tersebut dapat
+          terhirup oleh kita
+        </p>
+      </li>
+      <li class="flex flex-row">
+        <p class="mr-2">5.</p>
+        <p>
+          Hindari menyentuh mata, hidung dan mulut. Tangan menyentuh banyak hal
+          yang dapat terkontaminasi virus. Jika kita menyentuh mata, hidung dan
+          mulut dengan tangan yang terkontaminasi, maka virus dapat dengan mudah
+          masuk ke tubuh kita.
+        </p>
+      </li>
+      <li class="flex flex-row">
+        <p class="mr-2">6.</p>
+        <p>
+          Gunakan masker penutup mulut dan hidung ketika Anda sakit atau saat
+          berada di tempat umum.
+        </p>
+      </li>
+      <li class="flex flex-row">
+        <p class="mr-2">7.</p>
+        <p>
+          Buang tisu dan masker yang sudah digunakan ke tempat sampah, lalu
+          cucilah tangan Anda.
+        </p>
+      </li>
+      <li class="flex flex-row">
+        <p class="mr-2">8.</p>
+        <p>
+          Hindari kontak dengan hewan ternak dan hewan liar yang terbukti
+          tertular coronavirus.
+        </p>
+      </li>
+      <li class="flex flex-row">
+        <p class="mr-2">9.</p>
+        <p>Jangan makan daging yang tidak dimasak hingga matang.</p>
+      </li>
+      <li class="flex flex-row">
+        <p class="mr-2">10.</p>
+        <p>
+          Menunda perjalanan ke daerah/negara dimana virus ini ditemukan seperti
+          Tiongkok, seiring dengan informasi adanya penghentian sementara
+          operasional penerbangan langsung dari dan ke daratan Cina dari
+          pemerintah, sampai ada informasi lebih lanjut.
+        </p>
+      </li>
+    </ul>
+  </div>
+</template>
+

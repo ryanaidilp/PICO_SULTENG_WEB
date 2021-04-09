5ab2dd2e4a6cd3afec18926e128882a3d2d34d69
ryanaidilp <fajrianaidilp@gmail.com>
2021-04-09 21:30:43 +0800

    ref : update new chart implementation

--- resources/js/components/_pages/data/chart/NewCase.vue
+++ resources/js/components/_pages/data/chart/NewCase.vue
@@ -4,17 +4,17 @@
     <div class="bg-white rounded-lg shadow-lg vld-parent">
       <div class="flex justify-center w-full">
         <loading
-          :active="isLoading"
+          :active="isLoading()"
           :is-full-page="false"
           :opacity="0.8"
           :width="120"
-          height="400px"
+          :height="400"
           loader="bars"
           color="#59F"
         >
         </loading>
       </div>
-      <div v-show="!isLoading" style="height: 400px" class="p-5">
+      <div v-show="!isLoading()" style="height: 400px" class="p-5">
         <keep-alive>
           <canvas
             id="chart-bar"
@@ -56,36 +56,36 @@ var dataChart = {
         borderColor: "rgba(245, 171, 53, 0.8)",
         backgroundColor: "rgba(245, 171, 53, 1)",
       },
-      {
-        data: [],
-        label: "Negatif",
-        borderColor: "rgba(118, 93, 105, 0.8)",
-        backgroundColor: "rgba(118, 93, 105, 1)",
-      },
-      {
-        data: [],
-        label: "ODP Baru",
-        borderColor: "rgba(65, 131, 215, 0.8)",
-        backgroundColor: "rgba(65, 131, 215, 1)",
-      },
-      {
-        data: [],
-        label: "ODP Selesai",
-        borderColor: "rgba(58, 83, 155, 0.8)",
-        backgroundColor: "rgba(58, 83, 155, 1)",
-      },
-      {
-        data: [],
-        label: "PDP Baru",
-        borderColor: "rgba(83, 51, 237, 0.8)",
-        backgroundColor: "rgba(83, 51, 237, 1)",
-      },
-      {
-        data: [],
-        label: "PDP Selesai",
-        borderColor: "rgba(102, 51, 153, 0.8)",
-        backgroundColor: "rgba(102, 51, 153, 1)",
-      },
+      // {
+      //   data: [],
+      //   label: "Negatif",
+      //   borderColor: "rgba(118, 93, 105, 0.8)",
+      //   backgroundColor: "rgba(118, 93, 105, 1)",
+      // },
+      // {
+      //   data: [],
+      //   label: "ODP Baru",
+      //   borderColor: "rgba(65, 131, 215, 0.8)",
+      //   backgroundColor: "rgba(65, 131, 215, 1)",
+      // },
+      // {
+      //   data: [],
+      //   label: "ODP Selesai",
+      //   borderColor: "rgba(58, 83, 155, 0.8)",
+      //   backgroundColor: "rgba(58, 83, 155, 1)",
+      // },
+      // {
+      //   data: [],
+      //   label: "PDP Baru",
+      //   borderColor: "rgba(83, 51, 237, 0.8)",
+      //   backgroundColor: "rgba(83, 51, 237, 1)",
+      // },
+      // {
+      //   data: [],
+      //   label: "PDP Selesai",
+      //   borderColor: "rgba(102, 51, 153, 0.8)",
+      //   backgroundColor: "rgba(102, 51, 153, 1)",
+      // },
     ],
   },
   options: {
@@ -151,7 +151,6 @@ export default {
   },
   data() {
     return {
-      isLoading: true,
       chart: null,
       jsonDataProvinsi: {},
     };
@@ -168,40 +167,42 @@ export default {
         options: chartData.options,
       });
     },
+    isLoading() {
+      return this.jsonDataProvinsi === undefined ? true : false;
+    },
     getStatistic: function () {
-      var data = this.jsonDataProvinsi.daftar_kabupaten;
+      var data = this.isLoading() ? [] : this.jsonDataProvinsi.daftar_kabupaten;
       var districtList = [];
       var districtPositive = [];
       var districtRecovered = [];
       var districtDeath = [];
-      var districtNegative = [];
-      var districtODP = [];
-      var districtFinishedODP = [];
-      var districtPDP = [];
-      var districtFinishedPDP = [];
+      // var districtNegative = [];
+      // var districtODP = [];
+      // var districtFinishedODP = [];
+      // var districtPDP = [];
+      // var districtFinishedPDP = [];
       for (let i = 0; i < data.length; i++) {
         districtList.push(data[i].nama);
         districtPositive.push(data[i].kasus_baru.positif);
         districtRecovered.push(data[i].kasus_baru.sembuh);
         districtDeath.push(data[i].kasus_baru.meninggal);
-        districtNegative.push(data[i].kasus_baru.negatif);
-        districtODP.push(data[i].kasus_baru.ODP);
-        districtFinishedODP.push(data[i].selesai.ODP);
-        districtPDP.push(data[i].kasus_baru.PDP);
-        districtFinishedPDP.push(data[i].selesai.PDP);
+        // districtNegative.push(data[i].kasus_baru.negatif);
+        // districtODP.push(data[i].kasus_baru.ODP);
+        // districtFinishedODP.push(data[i].selesai.ODP);
+        // districtPDP.push(data[i].kasus_baru.PDP);
+        // districtFinishedPDP.push(data[i].selesai.PDP);
       }
       dataChart.data.labels = districtList;
       dataChart.data.datasets[0].data = districtPositive;
       dataChart.data.datasets[1].data = districtRecovered;
       dataChart.data.datasets[2].data = districtDeath;
-      dataChart.data.datasets[3].data = districtNegative;
-      dataChart.data.datasets[4].data = districtODP;
-      dataChart.data.datasets[5].data = districtFinishedODP;
-      dataChart.data.datasets[6].data = districtPDP;
-      dataChart.data.datasets[7].data = districtFinishedPDP;
+      // dataChart.data.datasets[3].data = districtNegative;
+      // dataChart.data.datasets[4].data = districtODP;
+      // dataChart.data.datasets[5].data = districtFinishedODP;
+      // dataChart.data.datasets[6].data = districtPDP;
+      // dataChart.data.datasets[7].data = districtFinishedPDP;
       this.chart.update();
       this.chart.render();
-      this.isLoading = false;
     },
   },
   watch: {

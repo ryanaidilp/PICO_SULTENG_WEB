8f9a7f70ca75c4ac87637efc863491e6023ab018
ryanaidilp <fajrianaidilp@gmail.com>
2021-07-18 18:54:28 +0800

    ref : update home data component

--- resources/js/components/_pages/home/HomeData.vue
+++ resources/js/components/_pages/home/HomeData.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex flex-wrap justify-center mt-8 md:mt-16">
     <h3
-      class="w-full font-bold text-center text-md lg:text-left md:ml-10 md:text-3xl"
+      class="w-full font-bold text-center  text-md lg:text-left md:ml-8 md:text-3xl"
     >
       Situasi Terkini COVID-19 di Sulawesi Tengah
     </h3>
@@ -17,38 +17,43 @@
         <rect x="30" y="0" rx="3" ry="3" width="400" height="15" />
       </content-loader>
     </div>
-    <p
-      v-else
-      class="w-full h-10 text-xs text-center lg:text-left md:ml-10 text-muted md:text-lg"
-    >
-      Pembaruan Terakhir :
+    <p v-else class="w-full h-10 text-center lg:text-left md:ml-8 md:text-lg">
+      <span class="text-gray-500">Pembaruan Terakhir :</span>
       {{ lastUpdate }}
-    
+
       <br />
       <i class="text-xs text-gray-600">
         Sumber data :
         <a
           href="https://dinkes.sultengprov.go.id"
-          class="no-underline hover:no-underline hover:text-gray-800"
+          class="no-underline hover:underline hover:text-blue-800"
           >https://dinkes.sultengprov.go.id</a
         >
       </i>
     </p>
-    <data-tabs 
-    :national.sync="national"
-    :local.sync="local"
-    :districts="districts"
-    class="md:ml-8" />
+    <data-tabs
+      :national.sync="national"
+      :local.sync="local"
+      :national-vaccine="nationalVaccine"
+      :province-vaccine="provinceVaccine"
+      class="md:ml-8"
+    />
   </div>
 </template>
 <script>
-import DataTabs from '@/components/DataTabs'
+import DataTabs from "@/components/DataTabs";
 import { ContentLoader } from "vue-content-loader";
 const { format } = require("date-fns");
 const { formatToTimeZone } = require("date-fns-timezone");
 import { id } from "date-fns/locale";
 export default {
-  props: ["lastUpdate", 'national', 'local', "districts"],
+  props: [
+    "lastUpdate",
+    "national",
+    "local",
+    "nationalVaccine",
+    "provinceVaccine",
+  ],
   components: {
     ContentLoader,
     DataTabs,

09ff7e62220b7cd4b3dd623cb551e0951a59d627
ryanaidilp <fajrianaidilp@gmail.com>
2021-07-18 18:47:36 +0800

    ref : update UI of card suspect

--- resources/js/components/CardSuspect.vue
+++ resources/js/components/CardSuspect.vue
@@ -1,37 +1,47 @@
 <template>
-  <div class="flex flex-col flex-grow w-full lg:w-1/2 flex-shrinks">
-    <div class="flex-1 overflow-hidden">
-      <div class="p-5 rounded-lg">
-        <div class="flex flex-row items-center bg-white rounded-lg shadow-lg">
-          <div class="flex-1 text-center">
-            <div class="p-2 bg-white border-b-2 border-gray-500 rounded-tl-lg rounded-tr-lg">
-              <h5 class="font-bold text-gray-600 uppercase md:text-lg">{{ title }}</h5>
-            </div>
-            <div class="flex items-center mt-4 flex-column">
-              <div class="w-1/2">
-                <p class="text-xs md:text-base">Dalam {{ status }}</p>
-                <h3 class="text-3xl font-bold">{{ formatNumber(active_case) }}</h3>
-                <h3 class="text-sm font-bold">({{ newCaseFormat(new_case - new_finished_case) }})</h3>
-                <p class="mt-2 text-sm text-gray-600">({{ percentageFormat(activeCasePercentage) }})</p>
-              </div>
-              <div class="w-1/2">
-                <p class="text-xs md:text-base">Selesai {{ status }}</p>
-                <h3 class="text-3xl font-bold">{{ formatNumber(total_finished_case) }}</h3>
-                <h3 class="text-sm font-bold">({{ newCaseFormat(new_finished_case) }})</h3>
-                <p
-                  class="mt-2 text-sm text-gray-600"
-                >({{ percentageFormat(finishedCasePercentage) }})</p>
-              </div>
-            </div>
-            <div class="mb-4 text-center">
-              <p>Total</p>
-              <h3 class="text-3xl font-bold">{{ formatNumber(total_case) }}</h3>
-              <h3 class="text-sm font-bold">
-                <i class="fas fa-arrow-up"></i>
-                {{ formatNumber(new_case) }}
-              </h3>
-            </div>
+  <div class="flex flex-col flex-grow w-full flex-shrinks">
+    <div class="flex flex-row items-center bg-white rounded-lg shadow-lg">
+      <div class="flex-1 text-center">
+        <div
+          class="p-2 bg-white border-b-2 border-gray-500 rounded-tl-lg rounded-tr-lg "
+        >
+          <h5 class="font-bold text-gray-600 uppercase md:text-lg">
+            {{ title }}
+          </h5>
+        </div>
+        <div class="flex items-center mt-4 flex-column">
+          <div class="w-1/2">
+            <p class="text-xs md:text-base">Dalam {{ status }}</p>
+            <h3 class="text-3xl font-bold">
+              {{ formatNumber(active_case) }}
+            </h3>
+            <h3 class="text-sm font-bold">
+              ({{ newCaseFormat(new_case - new_finished_case) }})
+            </h3>
+            <p class="mt-2 text-sm text-gray-600">
+              ({{ percentageFormat(activeCasePercentage) }})
+            </p>
           </div>
+          <div class="w-1/2">
+            <p class="text-xs md:text-base">Selesai {{ status }}</p>
+            <h3 class="text-3xl font-bold">
+              {{ formatNumber(total_finished_case) }}
+            </h3>
+            <h3 class="text-sm font-bold">
+              ({{ newCaseFormat(new_finished_case) }})
+            </h3>
+            <p class="mt-2 text-sm text-gray-600">
+              ({{ percentageFormat(finishedCasePercentage) }})
+            </p>
+          </div>
+        </div>
+        <div class="mb-4 text-center">
+          <p>Total</p>
+          <h3 class="text-3xl font-bold">{{ formatNumber(total_case) }}</h3>
+          <h3 class="text-sm font-bold">
+            <i class="fas fa-arrow-up"></i>
+            {{ formatNumber(new_case) }}
+          </h3>
         </div>
       </div>
     </div>
@@ -47,21 +57,21 @@ export default {
     "active_case",
     "total_case",
     "total_finished_case",
-    "status"
+    "status",
   ],
   computed: {
-    finishedCasePercentage: function() {
+    finishedCasePercentage: function () {
       return this.percentage(
         this.$props.total_finished_case,
         this.$props.total_case
       );
     },
-    activeCasePercentage: function() {
+    activeCasePercentage: function () {
       return this.percentage(this.$props.active_case, this.$props.total_case);
-    }
+    },
   },
   methods: {
-    percentage: function(value, total) {
+    percentage: function (value, total) {
       let sum = value.toString().replace(".", "");
       let count = total.toString().replace(".", "");
       return ((parseInt(sum) / parseInt(count)) * 100).toFixed(2);
@@ -79,8 +89,8 @@ export default {
       return res;
     },
     formatNumber(value) {
-      return NumberFormat.format(value)
-    }
-  }
+      return NumberFormat.format(value);
+    },
+  },
 };
 </script>

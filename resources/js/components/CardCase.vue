62050a5aea2e856c175bfb16eac2dbf57ba93ab4
ryanaidilp <fajrianaidilp@gmail.com>
2021-07-18 18:46:45 +0800

    chore : update card case UI

--- resources/js/components/CardCase.vue
+++ resources/js/components/CardCase.vue
@@ -1,33 +1,27 @@
 <template>
-  <div class="w-full p-4 bg-white rounded-lg shadow-lg " :class="text_color">
+  <div class="w-full p-4 bg-white rounded-lg shadow-lg" :class="text_color">
     <h4 class="text-2xl font-bold text-center uppercase">{{ title }}</h4>
     <div class="flex flex-row mt-3 font-bold text-center">
       <div class="w-1/2">
-        <b class="text-xs text-gray-800">Sulawesi Tengah</b>
+        <b class="text-sm font-semibold text-gray-800">Sulawesi Tengah</b>
         <div class="text-2xl">
           {{ totalLocal }}
         </div>
         <div>
           <span class="px-2 pb-1 mr-1 rounded-full" :class="bg_color">
-            <i
-              class="text-xs text-white fas"
-              :class="icon(new_local)"
-            ></i>
+            <i class="text-xs text-white fas" :class="icon(new_local)"></i>
           </span>
           {{ newLocal }}
         </div>
       </div>
       <div class="w-1/2">
-        <b class="text-xs text-gray-800">Indonesia</b>
+        <b class="text-sm font-semibold text-gray-800">Indonesia</b>
         <div class="text-2xl">
           {{ totalNational }}
         </div>
         <div>
           <span class="px-2 pb-1 mr-1 rounded-full" :class="bg_color">
-            <i
-              class="text-xs text-white fas"
-              :class="icon(new_national)"
-            ></i>
+            <i class="text-xs text-white fas" :class="icon(new_national)"></i>
           </span>
           {{ newNational }}
         </div>

d80d81f3d202171da5bb1d798e884801ac2b35f9
ryanaidilp <fajrianaidilp@gmail.com>
2021-07-18 18:54:50 +0800

    ref : update home page

--- resources/js/Pages/Home/Index.vue
+++ resources/js/Pages/Home/Index.vue
@@ -1,10 +1,10 @@
 <template>
   <layout>
     <div class="w-full leading-normal xl:px-0 xl:mt-8">
-      <div class="flex flex-wrap justify-center md:ml-4">
+      <div class="flex flex-wrap justify-center">
         <banner
-          :banners="donations"
-          class="rounded-lg shadow-lg w-96 h-96 sm:w-screen xl:max-w-lg xl:w-1/2"
+          :banners="banners"
+          class="w-full px-4 rounded-lg shadow-lg  sm:px-0 h-96 xl:max-w-2xl xl:w-full"
         />
         <div
           class="flex flex-col flex-wrap w-full mt-4 xl:w-1/2 xl:ml-12 xl:-mt-3"
@@ -16,15 +16,16 @@
       <data-covid
         :national.sync="national"
         :local.sync="local"
-        :lastUpdate="lastUpdate"
-        :districts.sync="districts"
+        :lastUpdate="formatDate(local.national_case.date)"
+        :national-vaccine="nationalVaccine"
+        :province-vaccine="provinceVaccine"
         class="mt-8"
       />
       <covid-info class="mt-16 md:mx-auto" />
       <see-more-link :linkUrl="route('wiki')" />
       <section class="m-4 mt-4 md:mt-8">
         <div
-          class="flex flex-col items-stretch p-5 my-4 bg-white rounded-lg shadow-md"
+          class="flex flex-col items-stretch p-5 my-4 bg-white rounded-lg shadow-md "
         >
           <h3 class="text-lg lg:text-2xl">
             <strong>Daftar Rumah Sakit Rujukan di Sulawesi Tengah</strong>
@@ -39,20 +40,19 @@
           </p>
           <br />
           <br />
-          <ul class="mt-8 hospital-list">
+          <ul class="items-stretch block mt-8 lg:grid lg:grid-cols-3">
             <li v-for="(h, index) in hospitals" :key="index">
               <ContactListItem
-                :nama="h.nama"
-                :alamat="h.alamat"
-                :email="h.email"
-                :telepon="h.telepon"
+                :nama="h.name"
+                :alamat="h.address"
+                :contacts="h.contacts"
               />
             </li>
           </ul>
           <br />
           <inertia-link
             :href="route('contact')"
-            class="px-4 py-2 text-center text-blue-700 border-2 border-blue-500 border-solid rounded-lg md:self-center hover:bg-blue-200"
+            class="px-4 py-2 text-center text-blue-700 border-2 border-blue-500 border-solid rounded-lg  md:self-center hover:bg-blue-200"
             >Lihat Rumah Sakit Lainnya</inertia-link
           >
         </div>
@@ -77,7 +77,7 @@
           <ShareableItems :items="infographics" />
           <div class="pb-5 text-center md:pb-8">
             <inertia-link
-              class="inline-block px-4 py-2 mt-8 text-center text-blue-800 border-2 border-blue-500 border-solid rounded-lg md:self-center hover:bg-blue-200"
+              class="inline-block px-4 py-2 mt-8 text-center text-blue-800 border-2 border-blue-500 border-solid rounded-lg  md:self-center hover:bg-blue-200"
               :href="route('infographic')"
               >Lihat Selengkapnya</inertia-link
             >
@@ -103,16 +103,35 @@ import PartnerFooter from "@/Shared/PartnerFooter";
 import { Inertia } from "@inertiajs/inertia";
 
 export default {
-  props: [
-    "donations",
-    "lastUpdate",
-    "national",
-    "local",
-    "districts",
-    "hospitals",
-    "infographics",
-    "partners",
-  ],
+  props: {
+    national: {
+      type: Object,
+    },
+    local: {
+      type: Object,
+    },
+
+    hospitals: {
+      type: Array,
+    },
+    infographics: {
+      type: Array,
+    },
+    partners: {
+      type: Array,
+    },
+    nationalVaccine: {
+      type: Object,
+    },
+    provinceVaccine: {
+      type: Object,
+    },
+  },
+  data() {
+    return {
+      banners: [],
+    };
+  },
   components: {
     Layout,
     Donation,
@@ -126,34 +145,39 @@ export default {
     PartnerFooter,
   },
   methods: {
+    loadBanners() {
+      axios
+        .get(this.api(`banners`))
+        .then((response) => {
+          this.banners = response.data.data;
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    },
     reloadData() {
       setInterval(() => {
+        this.loadBanners();
         Inertia.reload({
           only: [
             "donations",
             "lastUpdate",
-            "districts",
             "local",
             "national",
             "infographics",
+            "nationalVaccine",
+            "provinceVaccine",
           ],
         });
-      },5 * 60 * 1000);
+      }, 5 * 60 * 1000);
     },
   },
   mounted() {
+    this.loadBanners();
     this.reloadData();
   },
 };
 </script>
 
 <style lang="scss" scoped>
-.hospital-list {
-  display: block;
-  @screen lg {
-    display: grid;
-    grid-template-columns: repeat(3, 1fr);
-    align-items: stretch;
-  }
-}
 </style>

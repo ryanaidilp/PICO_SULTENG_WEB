cb3f20aecc90587f1fc563baf33bb3999b474a7f
ryanaidilp <fajrianaidilp@gmail.com>
2021-07-21 18:31:37 +0800

    chore : add vaccine locations

---
+++ database/seeders/VaccineLocationSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Carbon\Carbon;
+use App\Models\VaccineLocation;
+use Illuminate\Database\Seeder;
+
+class VaccineLocationSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $file = file(\database_path("csvs/vaccine_locations.csv"));
+        $data = \array_map("str_getcsv", $file);
+        $keys = $data[0];
+        \array_shift($data);
+        foreach ($data as $row) {
+            $row[6] = empty($row[6]) ? null : $row[6];
+            $row[7] = empty($row[7]) ? null : $row[7];
+            $row[8] = empty($row[8]) ? null : $row[8];
+            $row[9] = Carbon::parse($row[9]);
+            $row = array_combine($keys, $row);
+            VaccineLocation::create($row);
+        }
+    }
+}

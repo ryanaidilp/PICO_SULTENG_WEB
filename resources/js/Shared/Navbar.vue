fb03a643e6d3e4ff14c7a513f3c97109ac7686fb
ryanaidilp <fajrianaidilp@gmail.com>
2021-07-23 00:27:04 +0800

    ref : update navbar

--- resources/js/Shared/Navbar.vue
+++ resources/js/Shared/Navbar.vue
@@ -20,11 +20,11 @@
               />
             </div>
             <div>
-              <span class="text-xs font-semibold text-gray-800 md:text-lg"
+              <span class="text-xs font-bold text-gray-800 md:text-lg"
                 >Pusat Informasi COVID-19</span
               >
               <br />
-              <span class="text-xs text-gray-800 md:text-base"
+              <span class="text-xs text-gray-800 md:text-sm"
                 >Provinsi Sulawesi Tengah</span
               >
             </div>
@@ -110,17 +110,22 @@
 
             <div class="block pr-4 lg:hidden">
               <button
+                @click="toggleMenu"
                 ref="nav-toggle"
                 class="flex items-center px-3 py-2 text-gray-500 border border-gray-600 rounded appearance-none  hover:text-gray-900 hover:border-blue-500 focus:outline-none"
               >
-                <svg
-                  class="w-3 h-3 fill-current"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <title>Menu</title>
-                  <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-                </svg>
+                <transition name="fade" mode="out-in">
+                  <svg
+                    v-if="!showDropdown"
+                    class="w-3 h-3 fill-current"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <title>Menu</title>
+                    <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+                  </svg>
+                  <i v-else class="fas fa-times hover:text-blue-500"></i>
+                </transition>
               </button>
             </div>
           </div>
@@ -165,42 +170,75 @@
                 </inertia-link>
               </li>
               <li class="flex-1 mr-3">
-                <inertia-link
-                  :href="route('wiki')"
-                  :class="isActive('wiki')"
-                  @click.prevent="changePage(route('wiki'))"
+                <a
+                  href="#"
+                  @click.prevent="toggleMenu"
                   class="block py-1 pl-1 text-white no-underline align-middle border-b-2 border-gray-800  hover:text-blue-500 hover:border-blue-500"
                 >
-                  <i class="pr-0 fas fa-question"></i>
-                  <span class="block pb-1 text-xs">Wiki</span>
-                </inertia-link>
+                  <i class="fas fa-ellipsis-h"></i>
+                  <span class="block pb-1 text-xs">Lainnya</span>
+                </a>
               </li>
             </ul>
           </div>
         </div>
 
-        <div
-          class="z-20 flex-grow hidden w-full mt-2 bg-white lg:hidden"
-          ref="nav-content"
-        >
-          <ul class="items-center flex-1 px-4 list-reset lg:flex md:px-0">
-            <li class="my-2 mr-6 md:my-0">
-              <a
-                href="https://banuacoders.com/app/pico"
-                target="_blank"
-                class="block py-2 text-center text-white no-underline align-middle bg-blue-500 rounded-lg  hover:text-gray-900 hover:bg-blue-700"
-              >
-                <span class="px-3 text-sm md:pb-0">Download App</span>
-              </a>
-            </li>
-          </ul>
-        </div>
+        <transition name="fade" mode="out-in">
+          <div
+            v-if="showDropdown"
+            class="z-20 flex-grow w-full mt-2 bg-white border-t-2 border-gray-300  lg:hidden"
+            ref="nav-content"
+          >
+            <h2 class="m-2 font-semibold">Menu</h2>
+            <hr />
+            <ul class="items-center flex-1 px-1 list-reset lg:flex md:px-0">
+              <li class="my-2 md:my-0">
+                <inertia-link
+                  :href="route('vaccine')"
+                  :class="isActive('vaccine')"
+                  @click.prevent="changePage(route('vaccine'))"
+                  target="_blank"
+                  class="flex justify-between py-2 text-center text-gray-600 no-underline align-middle border-b-2 border-gray-300  hover:text-blue-500 hover:border-blue-500"
+                >
+                  <i class="fas fa-stethoscope"></i>
+                  <span class="px-3">Vaksin</span>
+                </inertia-link>
+              </li>
+              <li class="my-2 md:my-0">
+                <inertia-link
+                  :href="route('wiki')"
+                  :class="isActive('wiki')"
+                  @click.prevent="changePage(route('wiki'))"
+                  target="_blank"
+                  class="flex justify-between py-2 text-center text-gray-600 no-underline align-middle border-b-2 border-gray-300  hover:text-blue-500 hover:border-blue-500"
+                >
+                  <i class="pr-0 fas fa-question"></i>
+                  <span class="px-3">Wiki</span>
+                </inertia-link>
+              </li>
+              <li class="mx-auto my-2 md:my-0">
+                <a
+                  href="https://banuacoders.com/app/pico"
+                  target="_blank"
+                  class="block py-2 text-center text-white no-underline align-middle bg-blue-500 rounded-lg  hover:bg-blue-700"
+                >
+                  <span class="px-3 text-sm md:pb-0">Download App</span>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </transition>
       </div>
     </nav>
   </div>
 </template>
 <script>
 export default {
+  data() {
+    return {
+      showDropdown: false,
+    };
+  },
   methods: {
     isActive(link) {
       return this.route().current(link)
@@ -211,42 +249,45 @@ export default {
       this.$emit("change-page");
       this.$inertia.visit(page);
     },
+    toggleMenu() {
+      this.showDropdown = !this.showDropdown;
+    },
   },
   mounted() {
     var navMenuDiv = this.$refs["nav-content"];
     var navMenu = this.$refs["nav-toggle"];
 
-    document.onclick = check;
+    // document.onclick = check;
 
-    function check(e) {
-      var target = (e && e.target) || (event && event.srcElement);
+    // function check(e) {
+    //   var target = (e && e.target) || (e && e.srcElement);
 
-      //Nav Menu
-      if (!checkParent(target, navMenuDiv)) {
-        // click NOT on the menu
-        if (checkParent(target, navMenu)) {
-          // click on the link
-          if (navMenuDiv.classList.contains("hidden")) {
-            navMenuDiv.classList.remove("hidden");
-          } else {
-            navMenuDiv.classList.add("hidden");
-          }
-        } else {
-          // click both outside link and outside menu, hide menu
-          navMenuDiv.classList.add("hidden");
-        }
-      }
-    }
+    //   //Nav Menu
+    //   if (!checkParent(target, navMenuDiv)) {
+    //     // click NOT on the menu
+    //     if (checkParent(target, navMenu)) {
+    //       // click on the link
+    //       if (navMenuDiv.classList.contains("hidden")) {
+    //         navMenuDiv.classList.remove("hidden");
+    //       } else {
+    //         navMenuDiv.classList.add("hidden");
+    //       }
+    //     } else {
+    //       // click both outside link and outside menu, hide menu
+    //       navMenuDiv.classList.add("hidden");
+    //     }
+    //   }
+    // }
 
-    function checkParent(t, elm) {
-      while (t.parentNode) {
-        if (t == elm) {
-          return true;
-        }
-        t = t.parentNode;
-      }
-      return false;
-    }
+    // function checkParent(t, elm) {
+    //   while (t.parentNode) {
+    //     if (t == elm) {
+    //       return true;
+    //     }
+    //     t = t.parentNode;
+    //   }
+    //   return false;
+    // }
   },
 };
 </script>
@@ -254,5 +295,12 @@ export default {
 <style scoped>
 .main-nav-top {
   z-index: 200;
+}
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
 }
 </style>

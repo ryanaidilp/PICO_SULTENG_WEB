6050a80cfd21b30ec1e74c4092cb3f1b920c4b32
ryanaidilp <fajrianaidilp@gmail.com>
2021-07-24 23:20:05 +0800

    chore : create donation transformer

---
+++ app/Transformers/DonationTransformer.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Transformers;
+
+use App\Models\Donation;
+use League\Fractal\TransformerAbstract;
+use App\Transformers\ContactTransformer;
+use App\Transformers\BankAccountTransformer;
+
+class DonationTransformer extends TransformerAbstract
+{
+    /**
+     * List of resources to automatically include
+     *
+     * @var array
+     */
+    protected $defaultIncludes = [
+        "accounts", "contacts"
+    ];
+
+    /**
+     * List of resources possible to include
+     *
+     * @var array
+     */
+    protected $availableIncludes = [
+        //
+    ];
+
+    /**
+     * A Fractal transformer.
+     *
+     * @return array
+     */
+    public function transform(Donation $donation)
+    {
+        return [
+            "id" => (int) $donation->id,
+            "title" => $donation->title,
+            "description" => $donation->description,
+            "initiator" => $donation->initiator,
+            "url" => $donation->url,
+            "image" => $donation->image,
+            "accept_noncash" => $donation->accept_noncash,
+            "delivery_address" => $donation->delivery_address ?? "",
+        ];
+    }
+
+    public function includeAccounts(Donation $donation)
+    {
+        return $this->collection($donation->bank_accounts, new BankAccountTransformer);
+    }
+
+    public function includeContacts(Donation $donation)
+    {
+        return $this->collection($donation->contacts, new ContactTransformer);
+    }
+}

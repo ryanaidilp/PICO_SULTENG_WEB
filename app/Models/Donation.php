3a60181b3cde0c663ca4a908a95b5207feaec0dd
ryanaidilp <fajrianaidilp@gmail.com>
2021-07-24 23:16:34 +0800

    chore : add donation model

---
+++ app/Models/Donation.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Contact;
+use App\Models\BankAccount;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Donation extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+
+    // Relationships
+    public function contacts()
+    {
+        return $this->morphMany(Contact::class, "contactable");
+    }
+
+    public function bank_accounts()
+    {
+        return $this->morphMany(BankAccount::class, "accountable");
+    }
+
+    // Methods
+
+    // Mutators & Accessors
+}

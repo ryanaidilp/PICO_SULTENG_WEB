6bfb5502ce25bd22be8ba87e41f999d8f171da53
ryanaidilp <fajrianaidilp@gmail.com>
2021-07-24 23:21:46 +0800

    chore : create contact button component

---
+++ resources/js/components/ContactButton.vue
@@ -0,0 +1,47 @@
+<template>
+  <a
+    class="inline-block px-4 py-1 text-white transition-all duration-300 rounded  hover:opacity-50"
+    :href="setHref(contact)"
+    target="_blank"
+    :title="setLinkTitle(contact)"
+    :class="setBgColor(contact)"
+  >
+    <i class="mr-1 -ml-2" :class="setIcon(contact)" />
+    <span class="font-semibold">{{ contact.contact }}</span>
+  </a>
+</template>
+
+<script>
+export default {
+  props: {
+    contact: {
+      type: Object,
+      required: true,
+    },
+    textSize: {
+      type: String,
+      required: false,
+      default: "text-xs",
+    },
+    marginRight: {
+      type: String,
+      required: false,
+      default: "",
+    },
+  },
+  methods: {
+    setHref(contact) {
+      return `${contact.prefix}${contact.contact}`;
+    },
+    setLinkTitle(contact) {
+      return `${contact.label} ${contact.contact}`;
+    },
+    setIcon(contact) {
+      return contact.icon;
+    },
+    setBgColor(contact) {
+      return `${contact.bg_color} ${this.textSize} ${this.marginRight}`;
+    },
+  },
+};
+</script>

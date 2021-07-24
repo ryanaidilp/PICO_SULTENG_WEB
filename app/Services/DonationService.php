385561754ed85736bf4de58984208d897aaac24b
ryanaidilp <fajrianaidilp@gmail.com>
2021-07-24 23:20:18 +0800

    chore : create donation service

---
+++ app/Services/DonationService.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Donation;
+use App\Transformers\AppSerializer;
+use App\Transformers\DonationTransformer;
+
+class DonationService
+{
+    public function all()
+    {
+        $donations = Donation::with(["contacts", "contacts.contact_type", "bank_accounts", "bank_accounts.bank"])->get();
+        $donations = \fractal($donations, new DonationTransformer, new AppSerializer)->toArray();
+        return $donations;
+    }
+}

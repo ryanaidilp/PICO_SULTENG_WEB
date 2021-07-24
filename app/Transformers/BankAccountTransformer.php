50317419a20c1acf00f3ab640c3fc43ab56ceaa3
ryanaidilp <fajrianaidilp@gmail.com>
2021-07-24 23:19:53 +0800

    chore : create bank account transformer

---
+++ app/Transformers/BankAccountTransformer.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Transformers;
+
+use App\Models\BankAccount;
+use League\Fractal\TransformerAbstract;
+
+class BankAccountTransformer extends TransformerAbstract
+{
+    /**
+     * List of resources to automatically include
+     *
+     * @var array
+     */
+    protected $defaultIncludes = [
+        //
+    ];
+
+    /**
+     * List of resources possible to include
+     *
+     * @var array
+     */
+    protected $availableIncludes = [
+        //
+    ];
+
+    /**
+     * A Fractal transformer.
+     *
+     * @return array
+     */
+    public function transform(BankAccount $bank_account)
+    {
+        return [
+            "id"   => (int) $bank_account->id,
+            "name" => $bank_account->account_name,
+            "number" => $bank_account->account_number,
+            "bank"  => $bank_account->bank->name,
+            "bank_logo" => $bank_account->bank->logo,
+        ];
+    }
+}

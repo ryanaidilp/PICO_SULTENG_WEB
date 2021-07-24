e554ca12eff10c3614bab76c43b2cd2de69486b4
ryanaidilp <fajrianaidilp@gmail.com>
2021-07-24 23:16:55 +0800

    chore : create contact transformer

---
+++ app/Transformers/ContactTransformer.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Transformers;
+
+use App\Models\Contact;
+use League\Fractal\TransformerAbstract;
+
+class ContactTransformer extends TransformerAbstract
+{
+    /**
+     * List of resources to automatically include
+     *
+     * @var array
+     */
+    protected $defaultIncludes = [
+        //
+    ];
+
+    /**
+     * List of resources possible to include
+     *
+     * @var array
+     */
+    protected $availableIncludes = [
+        //
+    ];
+
+    /**
+     * A Fractal transformer.
+     *
+     * @return array
+     */
+    public function transform(Contact $contact)
+    {
+        return [
+            "id" => (int) $contact->id,
+            "contact_type_id" => $contact->contact_type_id,
+            "contact" => $contact->contact,
+            "prefix" => $contact->contact_type->prefix,
+            "bg_color" => $contact->contact_type->bg_color,
+            "icon" => $contact->contact_type->icon,
+            "label" => $contact->contact_type->label,
+        ];
+    }
+}

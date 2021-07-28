f986a420763fd26588077adef4aed8deffda9c74
ryanaidilp <fajrianaidilp@gmail.com>
2021-07-28 14:48:37 +0800

    chore : add scheduler to run command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,9 @@
 
 namespace App\Console;
 
+use App\Console\Commands\UpdateData;
+use App\Console\Commands\UpdateVaccine;
+use App\Console\Commands\UpdateProvince;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -13,18 +16,24 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //
+        UpdateData::class,
+        UpdateProvince::class,
+        UpdateVaccine::class
     ];
 
     /**
-     * Define the application's command schedule.
+     * Define the application"s command schedule.
      *
      * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
      * @return void
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command("covid:update")->dailyAt("18:00");
+        $schedule->command("covid:province")->dailyAt("18:00")->hourly()->between("18:00", "19:00");
+        $schedule->command("vaccine:update")->dailyAt("18:00")->hourly()->between("18:00", "19:00");
+        $schedule->command("cache:clear")->dailyAt("02:00");
+        $schedule->command("view:clear")->dailyAt("02:00");
     }
 
     /**
@@ -34,8 +43,8 @@ class Kernel extends ConsoleKernel
      */
     protected function commands()
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . "/Commands");
 
-        require base_path('routes/console.php');
+        require base_path("routes/console.php");
     }
 }

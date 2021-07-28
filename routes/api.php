c6ca98c31c550696d3a4cf0dc3902489a21e7529
ryanaidilp <fajrianaidilp@gmail.com>
2021-07-28 11:52:33 +0800

    chore : add internal api routes

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,18 @@ use Illuminate\Support\Facades\Route;
 Route::middleware("auth:api")->get("/user", function (Request $request) {
     return $request->user();
 });
+
+
+Route::prefix("v1")->group(function () {
+    Route::prefix("statistics")->group(function () {
+        Route::get("/", [App\Http\Controllers\Api\v1\StatisticController::class, "index"]);
+        Route::get("/latest", [App\Http\Controllers\Api\v1\StatisticController::class, "latest"]);
+        Route::get("/{day}", [App\Http\Controllers\Api\v1\StatisticController::class, "show"]);
+    });
+    Route::prefix("regencies")->group(function () {
+        Route::get("/all/{province_id}", [App\Http\Controllers\Api\v1\RegencyCaseController::class, "all"]);
+    });
+    Route::prefix("vaccine")->group(function () {
+        Route::get("/latest", [App\Http\Controllers\Api\v1\VaccineController::class, "latest"]);
+    });
+});

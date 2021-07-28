d06b606d11713afcaaafdb5199d9e2c566f5a79e
ryanaidilp <fajrianaidilp@gmail.com>
2021-07-28 12:00:16 +0800

    chore : create loader for vaccine card

---
+++ resources/js/components/loaders/CardVaccineLoader.vue
@@ -0,0 +1,28 @@
+<template>
+  <content-loader
+    :height="1456"
+    :width="1504"
+    :speed="2"
+    primaryColor="#f3f3f3"
+    secondaryColor="#c0c0c0"
+  >
+    <rect x="0" y="0" rx="10" ry="10" width="742" height="336" />
+    <rect x="762" y="0" rx="10" ry="10" width="742" height="336" />
+    <rect x="0" y="356" rx="10" ry="10" width="742" height="336" />
+    <rect x="762" y="356" rx="10" ry="10" width="742" height="336" />
+    <rect x="0" y="712" rx="10" ry="10" width="742" height="336" />
+    <rect x="762" y="712" rx="10" ry="10" width="742" height="336" />
+    <rect x="0" y="1072" rx="10" ry="10" width="742" height="336" />
+    <rect x="762" y="1072" rx="10" ry="10" width="742" height="336" />
+  </content-loader>
+</template>
+
+<script>
+import { ContentLoader } from "vue-content-loader";
+
+export default {
+  components: {
+    ContentLoader,
+  },
+};
+</script>
---
+++ resources/js/components/loaders/CardVaccineTargetLoader.vue
@@ -0,0 +1,22 @@
+<template>
+  <content-loader
+    :height="308"
+    :width="1504"
+    :speed="2"
+    primaryColor="#f3f3f3"
+    secondaryColor="#ecebeb"
+  >
+    <rect x="0" y="0" rx="10" ry="10" width="742" height="144" />
+    <rect x="762" y="0" rx="10" ry="10" width="742" height="144" />
+    <rect x="0" y="162" rx="10" ry="10" width="742" height="144" />
+    <rect x="762" y="162" rx="10" ry="10" width="742" height="144" />
+  </content-loader>
+</template>
+<script>
+import { ContentLoader } from "vue-content-loader";
+export default {
+  components: {
+    ContentLoader,
+  },
+};
+</script>

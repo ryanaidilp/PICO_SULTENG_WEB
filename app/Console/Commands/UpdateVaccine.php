f0e96beff71893e5635cb54985cb3c360f2aa753
ryanaidilp <fajrianaidilp@gmail.com>
2021-08-16 07:10:29 +0800

    ref : update vaccine:update command

--- app/Console/Commands/UpdateVaccine.php
+++ app/Console/Commands/UpdateVaccine.php
@@ -42,28 +42,73 @@ class UpdateVaccine extends Command
     public function handle()
     {
         $vaccine = NationalVaccine::latest()->first();
-        $data = Http::get("https://data.covid19.go.id/public/api/pemeriksaan-vaksinasi.json");
-        $data = $data["vaksinasi"]["penambahan"];
-        $date = Carbon::parse($data["tanggal"]);
+        $data = Http::get("https://cekdiri.id/vaksinasi");
+        $last_update = Carbon::parse($data["last_updated"]);
+        $data = \collect($data["monitoring"])->last();
+        $date = Carbon::parse($data["date"]);
+
         if ($date->isAfter($vaccine->date)) {
             NationalVaccine::create([
                 "day" => $vaccine->day + 1,
-                "date" => Carbon::parse($data["created"]),
-                "total_vaccination_target" => $vaccine->total_vaccination_target,
-                "first_vaccination_received" => $data["jumlah_vaksinasi_1"],
-                "second_vaccination_received" => $data["jumlah_vaksinasi_2"],
-                "health_worker_vaccination_target" => $vaccine->health_worker_vaccination_target,
-                "health_worker_first_vaccination_received" => $vaccine->health_worker_first_vaccination_received,
-                "health_worker_second_vaccination_received" => $vaccine->health_worker_second_vaccination_received,
-                "elderly_vaccination_target" => $vaccine->elderly_vaccination_target,
-                "elderly_first_vaccination_received" => $vaccine->elderly_first_vaccination_received,
-                "elderly_second_vaccination_received" => $vaccine->elderly_second_vaccination_received,
-                "public_officer_vaccination_target" => $vaccine->public_officer_vaccination_target,
-                "public_officer_first_vaccination_received" => $vaccine->public_officer_first_vaccination_received,
-                "public_officer_second_vaccination_received" => $vaccine->public_officer_second_vaccination_received,
-
+                "date" => $last_update,
+                "total_vaccination_target" => $data["total_sasaran_vaksinasi"],
+                "cumulative_first_vaccination_received" => $data["vaksinasi1"],
+                "cumulative_second_vaccination_received" => $data["vaksinasi2"],
+                "health_worker_vaccination_target" => $data["sasaran_vaksinasi_sdmk"],
+                "cumulative_health_worker_first_vaccination_received" => $data["tahapan_vaksinasi"]["sdm_kesehatan"]["total_vaksinasi1"],
+                "cumulative_health_worker_second_vaccination_received" => $data["tahapan_vaksinasi"]["sdm_kesehatan"]["total_vaksinasi2"],
+                "elderly_vaccination_target" => $data["sasaran_vaksinasi_lansia"],
+                "cumulative_elderly_first_vaccination_received" => $data["tahapan_vaksinasi"]["lansia"]["total_vaksinasi1"],
+                "cumulative_elderly_second_vaccination_received" => $data["tahapan_vaksinasi"]["lansia"]["total_vaksinasi2"],
+                "public_officer_vaccination_target" => $data["sasaran_vaksinasi_petugas_publik"],
+                "cumulative_public_officer_first_vaccination_received" => $data["tahapan_vaksinasi"]["petugas_publik"]["total_vaksinasi1"],
+                "cumulative_public_officer_second_vaccination_received" => $data["tahapan_vaksinasi"]["petugas_publik"]["total_vaksinasi2"],
+                "public_vaccination_target" => $data["sasaran_vaksinasi_masyarakat_umum"],
+                "cumulative_public_first_vaccination_received" => $data["tahapan_vaksinasi"]["masyarakat_umum"]["total_vaksinasi1"],
+                "cumulative_public_second_vaccination_received" => $data["tahapan_vaksinasi"]["masyarakat_umum"]["total_vaksinasi2"],
+                "teenager_vaccination_target" => $data["sasaran_vaksinasi_kelompok_1217"],
+                "cumulative_teenager_first_vaccination_received" => $data["tahapan_vaksinasi"]["kelompok_usia_12_17"]["total_vaksinasi1"],
+                "cumulative_teenager_second_vaccination_received" => $data["tahapan_vaksinasi"]["kelompok_usia_12_17"]["total_vaksinasi2"],
             ]);
             $this->info("Berhasil memperbarui data vaksin");
+            Log::info("Berhasil menambahkan data vaksin!");
+        } else if ($date->isSameAs($vaccine->date)) {
+            $yesterday = NationalVaccine::where('day',  $vaccine->day - 1)->first();
+            $attributes = [
+                "date" => $last_update,
+                "total_vaccination_target" => $data["total_sasaran_vaksinasi"],
+                "first_vaccination_received" => $data["vaksinasi1"] - $yesterday->cumulative_first_vaccination_received,
+                "second_vaccination_received" => $data["vaksinasi2"] - $yesterday->cumulative_second_vaccination_received,
+                "health_worker_vaccination_target" => $data["sasaran_vaksinasi_sdmk"],
+                "health_worker_first_vaccination_received" => $data["tahapan_vaksinasi"]["sdm_kesehatan"]["total_vaksinasi1"] - $yesterday->cumulative_health_worker_first_vaccination_received,
+                "health_worker_second_vaccination_received" => $data["tahapan_vaksinasi"]["sdm_kesehatan"]["total_vaksinasi2"] - $yesterday->cumulative_health_worker_second_vaccination_received,
+                "elderly_vaccination_target" => $data["sasaran_vaksinasi_lansia"],
+                "elderly_first_vaccination_received" => $data["tahapan_vaksinasi"]["lansia"]["total_vaksinasi1"] - $yesterday->cumulative_elderly_first_vaccination_received,
+                "elderly_second_vaccination_received" => $data["tahapan_vaksinasi"]["lansia"]["total_vaksinasi2"] - $yesterday->cumulative_elderly_second_vaccination_received,
+                "public_officer_vaccination_target" => $data["sasaran_vaksinasi_petugas_publik"],
+                "public_officer_first_vaccination_received" => $data["tahapan_vaksinasi"]["petugas_publik"]["total_vaksinasi1"] - $yesterday->cumulative_public_officer_first_vaccination_received,
+                "public_officer_second_vaccination_received" => $data["tahapan_vaksinasi"]["petugas_publik"]["total_vaksinasi2"] - $yesterday->cumulative_public_officer_second_vaccination_received,
+                "public_vaccination_target" => $data["sasaran_vaksinasi_masyarakat_umum"],
+                "public_first_vaccination_received" => $data["tahapan_vaksinasi"]["masyarakat_umum"]["total_vaksinasi1"] - $vaccine->cumulative_public_first_vaccination_received,
+                "public_second_vaccination_received" => $data["tahapan_vaksinasi"]["masyarakat_umum"]["total_vaksinasi2"] - $vaccine->cumulative_public_second_vaccination_received,
+                "teenager_vaccination_target" => $data["sasaran_vaksinasi_kelompok_1217"],
+                "teenager_first_vaccination_received" => $data["tahapan_vaksinasi"]["kelompok_usia_12_17"]["total_vaksinasi1"] - $vaccine->cumulative_teenager_first_vaccination_received,
+                "teenager_second_vaccination_received" => $data["tahapan_vaksinasi"]["kelompok_usia_12_17"]["total_vaksinasi2"] - $vaccine->cumulative_teenager_second_vaccination_received,
+                "cumulative_first_vaccination_received" => $data["vaksinasi1"],
+                "cumulative_second_vaccination_received" => $data["vaksinasi2"],
+                "cumulative_health_worker_first_vaccination_received" => $data["tahapan_vaksinasi"]["sdm_kesehatan"]["total_vaksinasi1"],
+                "cumulative_health_worker_second_vaccination_received" => $data["tahapan_vaksinasi"]["sdm_kesehatan"]["total_vaksinasi2"],
+                "cumulative_elderly_first_vaccination_received" => $data["tahapan_vaksinasi"]["lansia"]["total_vaksinasi1"],
+                "cumulative_elderly_second_vaccination_received" => $data["tahapan_vaksinasi"]["lansia"]["total_vaksinasi2"],
+                "cumulative_public_officer_first_vaccination_received" => $data["tahapan_vaksinasi"]["petugas_publik"]["total_vaksinasi1"],
+                "cumulative_public_officer_second_vaccination_received" => $data["tahapan_vaksinasi"]["petugas_publik"]["total_vaksinasi2"],
+                "cumulative_public_first_vaccination_received" => $data["tahapan_vaksinasi"]["masyarakat_umum"]["total_vaksinasi1"],
+                "cumulative_public_second_vaccination_received" => $data["tahapan_vaksinasi"]["masyarakat_umum"]["total_vaksinasi2"],
+                "cumulative_teenager_first_vaccination_received" => $data["tahapan_vaksinasi"]["kelompok_usia_12_17"]["total_vaksinasi1"],
+                "cumulative_teenager_second_vaccination_received" => $data["tahapan_vaksinasi"]["kelompok_usia_12_17"]["total_vaksinasi2"],
+            ];
+            $vaccine->update($attributes);
+            Log::info("Memperbarui data vaksin tanggal $last_update!");
         } else {
             Log::error("Gagal memperbarui data vaksin!");
             $this->error("Gagal memperbarui data vaksin!");

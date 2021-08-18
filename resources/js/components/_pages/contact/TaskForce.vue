3c26e32715853e621d597959458cb17579c28f83
ryanaidilp <fajrianaidilp@gmail.com>
2021-08-18 08:19:07 +0800

    refactor(components-contact): update task force contact component

--- resources/js/components/_pages/contact/TaskForce.vue
+++ resources/js/components/_pages/contact/TaskForce.vue
@@ -9,20 +9,119 @@
         Sulawesi Tengah.
       </p>
       <content-loader v-if="taskForces.length <= 0"></content-loader>
-      <ul v-else class="items-stretch block gap-5 mt-8 lg:grid lg:grid-cols-3">
-        <li v-for="(taskForce, index) in taskForces" :key="index">
-          <ContactListItem
-            :regency="taskForce.regency"
-            :label="`Call Center & Hotline Gugus Tugas COVID-19 ${taskForce.regency}`"
-            :contacts="taskForce.contacts"
-          />
-        </li>
-      </ul>
+      <div v-else>
+        <div class="hidden w-full mx-auto mt-8 md:flex md:flex-col">
+          <div class="flex flex-row">
+            <div
+              class="
+                w-1/12
+                p-2
+                text-xl
+                font-semibold
+                text-center
+                border border-gray-500
+                rounded-tl-lg
+              "
+            >
+              No
+            </div>
+            <div
+              class="
+                w-4/12
+                p-2
+                text-xl
+                font-semibold
+                text-center
+                border border-gray-500
+              "
+            >
+              Kabupaten
+            </div>
+            <div
+              class="
+                w-7/12
+                p-2
+                text-xl
+                font-semibold
+                text-center
+                border border-gray-500
+                rounded-tr-lg
+              "
+            >
+              Hotline/Call Center
+            </div>
+          </div>
+          <div
+            class="flex flex-row"
+            v-for="(taskForce, index) in taskForces"
+            :key="index"
+          >
+            <div
+              class="
+                flex
+                items-center
+                justify-center
+                w-1/12
+                p-2
+                font-semibold
+                text-center
+                border border-gray-500
+              "
+              :class="index == 12 ? 'rounded-bl-lg' : ''"
+            >
+              {{ index + 1 }}
+            </div>
+            <div
+              class="
+                flex
+                items-center
+                justify-center
+                w-4/12
+                p-2
+                text-center
+                border border-gray-500
+              "
+            >
+              {{ taskForce.regency }}
+            </div>
+            <div
+              class="
+                grid
+                w-7/12
+                gap-4
+                p-2
+                border border-gray-500
+                lg:grid-cols-3
+                xl:grid-cols-5
+                md:grid-cols-2
+              "
+              :class="index == 12 ? 'rounded-br-lg' : ''"
+            >
+              <contact-button
+                v-for="contact in taskForce.contacts"
+                :key="contact.id"
+                :contact="contact"
+                is-task-force
+              />
+            </div>
+          </div>
+        </div>
+        <div class="items-stretch block mt-8 md:hidden">
+          <div v-for="(taskForce, index) in taskForces" :key="index">
+            <ContactListItem
+              :regency="taskForce.regency"
+              :label="`Call Center & Hotline Gugus Tugas COVID-19 ${taskForce.regency}`"
+              :contacts="taskForce.contacts"
+            />
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 <script>
 import { ContentLoader } from "vue-content-loader";
+import ContactButton from "@/components/ContactButton";
 import ContactListItem from "@/components/TaskForceContactListItem";
 export default {
   props: {
@@ -35,6 +134,7 @@ export default {
 
   components: {
     ContentLoader,
+    ContactButton,
     ContactListItem,
   },
 };

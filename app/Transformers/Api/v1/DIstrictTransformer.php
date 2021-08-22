25230e7523f46a682822a97f3b340e9cd34b5ccd
Fajrian Aidil Pratama <ryanaidilp@users.noreply.github.com>
2021-08-22 07:11:54 +0000

    Apply fixes from StyleCI

--- app/Transformers/Api/v1/DIstrictTransformer.php
+++ app/Transformers/Api/v1/DIstrictTransformer.php
@@ -7,23 +7,23 @@ use League\Fractal\TransformerAbstract;
 class DIstrictTransformer extends TransformerAbstract
 {
     /**
-     * List of resources to automatically include
+     * List of resources to automatically include.
      *
      * @var array
      */
     protected $defaultIncludes = [
         //
     ];
-    
+
     /**
-     * List of resources possible to include
+     * List of resources possible to include.
      *
      * @var array
      */
     protected $availableIncludes = [
         //
     ];
-    
+
     /**
      * A Fractal transformer.
      *
